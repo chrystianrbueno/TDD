cc9a56202856f86cab6edf3c05934cafe16a3578
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-07-16 17:57:27 -0300

    Criação de Suite e dados para testes

--- src/test/java/br/ce/wcaquino/servicos/CalculoValorLocacaoTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculoValorLocacaoTest.java
@@ -13,6 +13,8 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
+import br.ce.wcaquino.builders.FilmeBuilder;
+import br.ce.wcaquino.builders.UsuarioBuilder;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -22,51 +24,51 @@ import br.ce.wcaquino.exceptions.LocadoraException;
 @RunWith(Parameterized.class)
 public class CalculoValorLocacaoTest {
 	private LocacaoService service;
-	
+
 	@Parameter
 	public List<Filme> filmes;
-	
+
 	@Parameter(value = 1)
 	public Double valorLocacao;
-	
-	@Parameter(value=2)
+
+	@Parameter(value = 2)
 	public String cenario;
-	
+
 	@Before
 	public void setup() {
 		service = new LocacaoService();
 	}
-	
-	public static Filme filme1 = new Filme("Filme1", 2, 4.0);
-	public static Filme filme2 = new Filme("Filme2", 2, 4.0);
-	public static Filme filme3 = new Filme("Filme3", 2, 4.0);
-	public static Filme filme4 = new Filme("Filme4", 2, 4.0);
-	public static Filme filme5 = new Filme("Filme5", 2, 4.0);
-	public static Filme filme6 = new Filme("Filme6", 2, 4.0);
-	public static Filme filme7 = new Filme("Filme6", 2, 4.0);
-	
-	@Parameters(name="{2}")
-	public static Collection<Object []> getParametros(){
-		return Arrays.asList(new Object [][]{
-												{Arrays.asList(filme1, filme2), 8.0,"2 Filmes Sem Desconto"},
-												{Arrays.asList(filme1, filme2, filme3), 11.0,"3 Filmes 25%"},
-												{Arrays.asList(filme1, filme2, filme3, filme4), 13.0,"4 Filmes 50%"},
-												{Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0,"5 Filmes 75%"},
-												{Arrays.asList(filme1, filme2, filme3, filme4, filme5,filme6), 14.0,"6 Filmes 100%"},
-												{Arrays.asList(filme1, filme2, filme3, filme4, filme5,filme6,filme7), 18.0,"6 Filmes 100%"}
-											});
+
+	public static Filme filme1 = FilmeBuilder.umFilme().agora();
+	public static Filme filme2 = FilmeBuilder.umFilme().agora();
+	public static Filme filme3 = FilmeBuilder.umFilme().agora();
+	public static Filme filme4 = FilmeBuilder.umFilme().agora();
+	public static Filme filme5 = FilmeBuilder.umFilme().agora();
+	public static Filme filme6 = FilmeBuilder.umFilme().agora();
+	public static Filme filme7 = FilmeBuilder.umFilme().agora();
+
+	@Parameters(name = "{2}")
+	public static Collection<Object[]> getParametros() {
+		return Arrays.asList(new Object[][] { { Arrays.asList(filme1, filme2), 8.0, "2 Filmes Sem Desconto" },
+				{ Arrays.asList(filme1, filme2, filme3), 11.0, "3 Filmes 25%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4), 13.0, "4 Filmes 50%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0, "5 Filmes 75%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 14.0, "6 Filmes 100%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7), 18.0, "6 Filmes 100%" } });
 	}
-	
+
 	@Test
 	public void deveCalcularValorLocacaoConsiderandoDescontos() throws FilmeSemEstoqueException, LocadoraException {
-		//cenario
-		Usuario usuario = new Usuario("Usuario 1");
+		// cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
 
-		//acao 
+		// acao
 		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		//verificacao
+
+		// verificacao
 		Assert.assertThat(resultado.getValor(), CoreMatchers.is(valorLocacao));
+
+		System.out.println("!");
 	}
 
 }
--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -19,6 +19,8 @@ import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 
+import br.ce.wcaquino.builders.FilmeBuilder;
+import br.ce.wcaquino.builders.UsuarioBuilder;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -28,85 +30,83 @@ import br.ce.wcaquino.matchers.MatchersProprios;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
-	
+
 	private LocacaoService service;
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
-	
+
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
-	
+
 	@Before
 	public void setup() {
-		System.out.println("Before");
 		service = new LocacaoService();
 	}
-	
+
 	@Test
 	public void deveAlugarFilme() throws Exception {
 		Assume.assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
-		
-		//cenario
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme", 1, 5.0));
-		
-		//acao
+
+		// cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().comValor(5.0).agora());
+
+		// acao
 		Locacao locacao = service.alugarFilme(usuario, filmes);
 
-		//verificacao
+		// verificacao
 		error.checkThat(locacao.getValor(), CoreMatchers.is(5.0));
-		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true)) ;
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
 		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 		error.checkThat(locacao.getDataRetorno(), MatchersProprios.ehHojeComDiferencaDias(1));
 
 	}
-	
-	@Test(expected = FilmeSemEstoqueException.class)
-	public void naoDeveAlugarFilmeSemEstoque() throws Exception{
-		//cenario
-		Usuario usuario = new Usuario("Usuário 1");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme", 0, 5.0));
 
-		
-		//acao
+	@Test(expected = FilmeSemEstoqueException.class)
+	public void naoDeveAlugarFilmeSemEstoque() throws Exception {
+		// cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilmeSemEstoque().agora());
+
+		// acao
 		service.alugarFilme(usuario, filmes);
 	}
-	
+
 	@Test
 	public void naoDeveAlugarFilmeSemUsuario() throws FilmeSemEstoqueException {
-		List<Filme> filmes = Arrays.asList(new Filme("Filme", 1, 5.0));
-		
-			try {
-				service.alugarFilme(null, filmes);
-				Assert.fail();
-			} catch (LocadoraException e) {
-				// TODO Auto-generated catch block
-				Assert.assertThat(e.getMessage(), CoreMatchers.is("Usuario vazio"));
-			}
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().agora());
+
+		try {
+			service.alugarFilme(null, filmes);
+			Assert.fail();
+		} catch (LocadoraException e) {
+			// TODO Auto-generated catch block
+			Assert.assertThat(e.getMessage(), CoreMatchers.is("Usuario vazio"));
+		}
 	}
-	
+
 	@Test
 	public void naoDeveAlugarFilmeSemFilme() throws FilmeSemEstoqueException {
-		Usuario usuario = new Usuario("Usuário 1");
-			try {
-				service.alugarFilme(usuario, null);
-				Assert.fail();
-			} catch (LocadoraException e) {
-				// TODO Auto-generated catch block
-				Assert.assertThat(e.getMessage(), CoreMatchers.is("Filme vazio"));
-			}
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		try {
+			service.alugarFilme(usuario, null);
+			Assert.fail();
+		} catch (LocadoraException e) {
+			// TODO Auto-generated catch block
+			Assert.assertThat(e.getMessage(), CoreMatchers.is("Filme vazio"));
+		}
 	}
-	
+
 	@Test
 	public void devolverNaSegunda() throws FilmeSemEstoqueException, LocadoraException {
 		Assume.assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
-		
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 1, 5.0));
-		
-		Locacao retorno =  service.alugarFilme(usuario, filmes);
-		
+
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().agora());
+
+		Locacao retorno = service.alugarFilme(usuario, filmes);
+
 		assertThat(retorno.getDataRetorno(), MatchersProprios.caiNumaSegunda());
 	}
-	
+
 }
